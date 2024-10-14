3e8fdfcd7f8adfa97c59941ff26877f99eba9293
Lugtu, Baron Olandria <lugtubaron@gmail.com>
2024-10-14 18:05:26 +0800

    Consolidated repository.

---
+++ FileProcessorAPI/DataProcess/ReportService.cs
@@ -0,0 +1,28 @@
+﻿using FileProcessorAPI.Models;
+
+namespace FileProcessorAPI.DataProcess
+{
+    public class ReportService : BackgroundService
+    {
+        public ReportService() { }
+
+        private readonly PeriodicTimer _periodicTimer = new PeriodicTimer(TimeSpan.FromMilliseconds(1000));
+
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (await _periodicTimer.WaitForNextTickAsync(stoppingToken))
+            {
+                await ProcessReportAsync(1);
+            }
+        }
+
+
+        public async Task  ProcessReportAsync(int reportType)
+        {
+            // Start the Processing the report here...
+            var report = new ReportData();
+        }
+    }
+}
---
+++ FileProcessorAPI/Models/ReportData.cs
@@ -0,0 +1,15 @@
+﻿namespace FileProcessorAPI.Models
+{
+    public class ReportData
+    {
+        public ReportData()
+        {
+            ReportType = 0;
+        }
+
+        public int ReportType { get; set; }
+        public string ReportName { get; set; }
+        public double DurationSeconds { get; set; } = 0.00;
+
+    }
+}
